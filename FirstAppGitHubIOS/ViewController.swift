d91ca0b484b8e4f072eee288d0517bae9b27a87f
Anton <ikelmimi@gmail.com>
2022-02-08 18:53:59 +0300

    Initial Commit

---
+++ FirstAppGitHubIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstAppGitHubIOS
+//
+//  Created by mbp15 on 8.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
